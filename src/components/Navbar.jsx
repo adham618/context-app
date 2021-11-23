993d8bf28af82738e9e7cf15987f8ed1ae312887
adham618 <adhamtarek291@gmail.com>
2021-11-23 16:11:08 +0200

    add navbar  component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-class extends React.Component {
+class Navbar extends React.Component {
   render() {
-    return <div></div>;
+    return <nav>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>;
   }
 }
 
-export default ;
+export default Navbar;
