4416bada7b071661d0888b4e82b2f08d76583640
adham618 <adhamtarek291@gmail.com>
2021-11-24 15:00:44 +0200

    add themecontext component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { ThemeContext } from "./Context/ThemeContext";
 class Navbar extends React.Component {
   render() {
-    return <nav>
-      <h1>Context App</h1>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>;
+    return (
+      <ThemeContext.Consumer>{(themecontext) => {
+        const { isLightTheme, light, dark } = themecontext;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <nav style={{ background: theme.ul, color: theme.syntax }
+          }>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>)
+      }}
+      </ThemeContext.Consumer>
+    );
   }
 }
 
