71c2a85a935aeb5cf00504e52a309c6337fe3fd3
adham618 <adhamtarek291@gmail.com>
2021-11-24 14:59:30 +0200

    create themecontext commponent

--- src/components/Context/ThemeContext.jsx
+++ src/components/Context/ThemeContext.jsx
@@ -7,8 +7,11 @@ class ThemeContextProvider extends React.Component {
     light: { syntax: "#555", ul: "#DDD", bg: "#eee" },
     dark: { syntax: "#ddd", ul: "#333", bg: "#555" }
   }
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  }
   render() {
-    return <ThemeContext.Provider value={{ ...this.state }}>
+    return <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
       {this.props.children}
     </ThemeContext.Provider>;
   }
