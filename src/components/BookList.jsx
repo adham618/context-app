6c7ae7cee081e71fe8dc0614bc4916593e2bf3e7
adham618 <adhamtarek291@gmail.com>
2021-11-24 15:03:31 +0200

    edit bookList component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { ThemeContext } from './Context/ThemeContext';
 class BookList extends React.Component {
+  static contextType = ThemeContext;
   render() {
-    return <div className="book-list">
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
       <ul>
-        <li>the way of kings</li>
-        <li>the name of the wind</li>
-        <li>the final empire</li>
+        <li style={{ background: theme.ul }}>the way of kings</li>
+        <li style={{ background: theme.ul }}>the name of the wind</li>
+        <li style={{ background: theme.ul }}>the final empire</li>
       </ul>
     </div>;
   }
