090b2aef37c0a81c8071df32bc13d9acc50cbb1a
adham618 <adhamtarek291@gmail.com>
2021-11-24 15:01:52 +0200

    add the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import ThemeContextProvider from './components/Context/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+        <ThemeToggle />
+      </ThemeContextProvider>
     </div>
   )
 }
