c233a9fd4ff281fb0a3b937942753a640b06322e
adham618 <adhamtarek291@gmail.com>
2021-11-25 14:36:11 +0200

    convert from class component to functional component

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './Context/ThemeContext';
-class ThemeToggle extends React.Component {
-  static contextType = ThemeContext;
-  render() {
-    const { toggleTheme } = this.context;
-    return (<button onClick={toggleTheme}>Toggle the theme</button>);
-  }
+
+const ThemeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  return (<button onClick={toggleTheme}>Toggle the theme</button>);
 }
 
 export default ThemeToggle;
