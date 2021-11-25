69f79f6b2a5210e10af1d71eb63b1518f1ac0fd0
adham618 <adhamtarek291@gmail.com>
2021-11-25 14:34:22 +0200

    create AuthContext with class component

---
+++ src/components/Context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+export const AuthContext = React.createContext();
+
+class AuthContextProvider extends React.Component {
+  state = {
+    isAuth: false
+  }
+  toggleAuth = () => {
+    this.setState({ isAuth: !this.state.isAuth });
+  }
+  render() {
+    return <AuthContext.Provider value={{ ...this.state, toggleAuth: this.toggleAuth }}>
+      {this.props.children}
+    </AuthContext.Provider>;
+  }
+}
+
+export default AuthContextProvider;
